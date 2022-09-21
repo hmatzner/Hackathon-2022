c56161a4158eb6b3338bbe5de0e253f359cdc85e
Kseniia Konoshko <ksushka.k.393@gmail.com>
2022-09-21 19:55:25 +0300

    'inference server code + model'

---
+++ AWS/inference.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import numpy as np
+import pickle
+from flask import Flask, request
+import shap
+
+app = Flask(__name__)
+
+
+def get_model(filename):
+    """Download model from pickle file"""
+    return pickle.load(open(filename, 'rb'))
+
+
+def get_prediction(model, X):
+    """Make prediction with the model"""
+    return model.predict(X)
+
+
+# download model
+filename = 'churn_model.pkl'
+model = get_model(filename)
+
+
+@app.route('/predict_churn', methods=['POST'])
+def predict_churn():
+    model = get_model(filename)
+    data = request.get_json(force=True)
+
+    df = pd.read_json(data)
+    explainer = shap.TreeExplainer(model)
+    shap_values = explainer.shap_values(df)
+
+    SAMPLE_NUMBER = 0
+    score = model.predict_proba(data)[:, 1][SAMPLE_NUMBER]
+    score = pd.DataFrame({'feature': 'score', 'importance': [score]})
+
+    ROUNDER = 5
+    POSITIVE_CLASS = 1
+    feat_name = pd.DataFrame(df.columns)
+    feat_values = pd.DataFrame(np.around(shap_values[POSITIVE_CLASS][SAMPLE_NUMBER], ROUNDER))
+    df_imp = pd.concat((feat_name, feat_values), axis=1)
+    df_imp.columns = ['feature', 'importance']
+    df_imp.sort_values(by='importance', inplace=True, ascending=False)
+
+    result = score.append(df_imp)
+
+    server_answer = result.to_json()
+    return server_answer
