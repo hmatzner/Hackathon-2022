72f691fd6bd71a12232ce64d1b6270c6007b4f27
Kseniia Konoshko <ksushka.k.393@gmail.com>
2022-09-21 23:56:17 +0300

    'inference modified'

--- AWS/inference.py
+++ AWS/inference.py
@@ -24,34 +24,30 @@ model = get_model(filename)
 
 @app.route('/predict_bankrupt', methods=['GET', 'POST'])
 def predict_bankrupt():
+    model = get_model(filename)
     data = request.get_json(force=True)
-    value = int(data['value'])
 
-    return jsonify({ "value": value + 1 })
-    # print(request.is_json)
-    # model = get_model(filename)
-    # data = request.get_json(force=True)
+    df = pd.read_json(data).T
+    explainer = shap.TreeExplainer(model)
+    shap_values = explainer.shap_values(df)
 
-    # df = pd.read_json(data)
-    # explainer = shap.TreeExplainer(model)
-    # shap_values = explainer.shap_values(df)
-    #
-    # SAMPLE_NUMBER = 0
-    # score = model.predict_proba(data)[:, 1][SAMPLE_NUMBER]
-    # score = pd.DataFrame({'feature': 'score', 'importance': [score]})
+    SAMPLE_NUMBER = 0
+    score = model.predict_proba(df)[:, 1][SAMPLE_NUMBER]
+    score = pd.DataFrame({'feature': 'score', 'importance': [score]})
 
-    # ROUNDER = 5
-    # POSITIVE_CLASS = 1
-    # feat_name = pd.DataFrame(df.columns)
-    # feat_values = pd.DataFrame(np.around(shap_values[POSITIVE_CLASS][SAMPLE_NUMBER], ROUNDER))
-    # df_imp = pd.concat((feat_name, feat_values), axis=1)
-    # df_imp.columns = ['feature', 'importance']
-    # df_imp.sort_values(by='importance', inplace=True, ascending=False)
-    #
-    # result = score.append(df_imp, ignore_index = True)
-    # server_answer = result.to_dict()
-    # server_answer = result.to_json()
-    # return jsonify(server_answer)
+    ROUNDER = 5
+    POSITIVE_CLASS = 1
+    feat_name = pd.DataFrame(df.columns)
+    feat_values = pd.DataFrame(np.around(shap_values[POSITIVE_CLASS][SAMPLE_NUMBER], ROUNDER))
+    df_imp = pd.concat((feat_name, feat_values), axis=1)
+    df_imp.columns = ['feature', 'importance']
+    df_imp.sort_values(by='importance', inplace=True, ascending=False)
+
+    result = score.append(df_imp, ignore_index = True)
+    server_answer = result.to_dict()
+    server_answer = result.to_json()
+
+    return jsonify(server_answer)
 
 
 if __name__ == '__main__':
